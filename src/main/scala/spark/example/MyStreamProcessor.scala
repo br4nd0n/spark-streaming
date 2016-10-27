17b378317898a45aca4bde1f70b447bce7099d69
Brandon O'Brien <a-bobrien@expedia.com>
2016-10-26 17:06:23 -0700

    adding stream transition graph metrics

--- src/main/scala/spark/example/MyStreamProcessor.scala
+++ src/main/scala/spark/example/MyStreamProcessor.scala
@@ -2,6 +2,7 @@ package spark.example
 
 import com.google.gson.Gson
 import org.apache.spark.streaming.dstream.DStream
+import java.util.Date
 
 /**
  * Created by brobrien on 10/26/16.
@@ -10,16 +11,65 @@ object MyStreamProcessor {
 
   val gson = new Gson()
 
+  val sourceName = "Source Stream"
+  val longWordsName = "Long Words"
+  val shortWordsName = "Short Words"
+
+  val longWordsPosName = "Positive Sentiment"
+  val longWordsNegName = "Negative Sentiment"
+
+  val persistedWordsName = "Persisted in Database"
+
+
+  //classify word based on first letter
+  def isPositiveSentiment(word: String): Boolean = {
+    val firstLetterASCII = word.charAt(0)
+    firstLetterASCII.toInt > 110
+  }
+
+  def isLongWord(word: String): Boolean = {
+    word.length > 3
+  }
+
   def processStreamCountWordLength(stream: DStream[String]): Unit = {
 
-    stream.foreachRDD(rdd => {
+    //Create complex DAG for illustration purposes
+    stream.foreachRDD(wordList => {
+      wordList.persist() //persist RDD to avoid unnecessary recomputation
 
-      val batchOfWords = rdd.collect() //normally you wouldn't do this in a production scenario
+      val longWords = wordList.filter(isLongWord(_))
+      val shortWords = wordList.filter(!isLongWord(_))
+
+      val longPosWords = longWords.filter(isPositiveSentiment(_))
+      val longNegWords = longWords.filter(!isPositiveSentiment(_))
+
+      //val shortPosWords = shortWords.filter(isPositiveSentiment(_))
+      //val shortNegWords = shortWords.filter(!isPositiveSentiment(_))
+
+      //Build logic representation of stream transition graph for later visualization
+      val s1 = StreamTransition(sourceName, longWordsName, longWords.count())
+      val s2 = StreamTransition(sourceName, shortWordsName, shortWords.count())
+      val s3 = StreamTransition(longWordsName, longWordsPosName, longPosWords.count())
+      val s4 = StreamTransition(longWordsName, longWordsNegName, longNegWords.count())
+      //val s5 = StreamTransition(shortWordsName, shortWordsPosName, shortPosWords.count())
+      //val s6 = StreamTransition(shortWordsName, shortWordsNegName, shortNegWords.count())
+      val s7 = StreamTransition(shortWordsName, persistedWordsName, shortWords.count())
+      val s8 = StreamTransition(longWordsPosName, persistedWordsName, longPosWords.count())
+      val s9 = StreamTransition(longWordsNegName, persistedWordsName, longNegWords.count())
+
+      val transitions = Array(s1, s2, s3, s4, s7, s8, s9)
+      val streamTransitionsJson = gson.toJson(transitions)
+      JedisProvider.exec(_.publish("stream-transitions", streamTransitionsJson))
+    })
+
+    //Compute Summary Metrics
+    stream.foreachRDD(wordList => {
+      val batchOfWords = wordList.collect() //normally you wouldn't do this in a production scenario
       val totalWords = batchOfWords.length
       val totalCharacters = batchOfWords.foldLeft(0) { (sum, word) => sum + word.length }
       val avgCharsPerWord = if (totalWords == 0) 0 else (totalCharacters.toDouble / totalWords)
 
-      val summaryMetrics = SummaryMetrics(totalWords, totalCharacters, avgCharsPerWord)
+      val summaryMetrics = SummaryMetrics(totalWords, totalCharacters, avgCharsPerWord, new Date())
 
       val jsonSummaryData = gson.toJson(summaryMetrics)
       println(jsonSummaryData)
@@ -32,6 +82,7 @@ object MyStreamProcessor {
       })
     })
 
+    //Compute Detail Metrics
     val wordLengths: DStream[(Int, Int)] = stream.map(word => (word.length, 1))
     val countsByLength: DStream[(Int, Int)] = wordLengths.reduceByKey((w1, w2) => w1 + w2)
 
@@ -70,8 +121,8 @@ object MyStreamProcessor {
   }
 }
 
-case class SummaryMetrics(totalWords: Int, totalCharacters: Int, avgCharsPerWord: Double)
+case class SummaryMetrics(totalWords: Int, totalCharacters: Int, avgCharsPerWord: Double, lastRDDTime: Date)
 
 case class DetailCount(wordLength: Int, count: Int)
 
-case class StreamTransition(from: String, to: String, count: Int)
+case class StreamTransition(from: String, to: String, count: Long)
